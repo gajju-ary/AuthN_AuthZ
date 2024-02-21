0548913ad8cd4d77ab22a2fe14c32227823ba052
GajendraChoudhary <gajendrachoudhary18@gmail.com>
2024-02-21 13:58:37 +0530

    Add project files.

---
+++ AuthN_AuthZ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthN_AuthZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
